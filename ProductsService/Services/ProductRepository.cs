0182067ec0b46a3ea5c8963c248d0c9718434a26
VictorJernestrand <56867809+VictorJernestrand@users.noreply.github.com>
2020-05-19 17:29:08 +0200

    added interface and repository for unit tests

---
+++ ProductsService/Services/ProductRepository.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductsService.Context;
+using ProductsService.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductsService.Services
+{
+    public class ProductRepository : IProductRepository
+    {
+        public ProductApiContext _context { get; set; }
+        public ProductRepository(ProductApiContext context)
+        {
+            _context = context;
+        }
+        public async Task<Product> GetById(int id)
+        {
+            return await _context.Products.FindAsync(id);
+        }
+
+        public async Task<List<Product>> GetAll()
+        {
+            return await _context.Products.ToListAsync();
+        }
+    }
+}
