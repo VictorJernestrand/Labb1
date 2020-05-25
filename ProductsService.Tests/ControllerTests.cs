6220640e5f6ff484603e51c0c834e14f48d9960c
VictorJernestrand <56867809+VictorJernestrand@users.noreply.github.com>
2020-05-25 15:00:48 +0200

    ProductFixture class

--- ProductsService.Tests/ControllerTests.cs
+++ ProductsService.Tests/ControllerTests.cs
@@ -1,14 +1,22 @@
-﻿using System;
+﻿using ProductsService.Models;
+using System;
 using System.Collections.Generic;
 using System.Net;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 using Xunit;
 
 namespace ProductsService.Tests
 {
-    public class ControllerTests
+    public class ControllerTests : IClassFixture<ProductFixture>
     {
+        ProductFixture _fixture;
+        public ControllerTests(ProductFixture fixture)
+        {
+            _fixture = fixture;
+        }
+
         [Fact]
         public async Task GetAllProducts_Returns_Ok()
         {
@@ -40,6 +48,22 @@ namespace ProductsService.Tests
                 var response = await client.GetAsync("/api/products/" + 1);
                 response.EnsureSuccessStatusCode();
                 Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            }
+        }
+        [Fact]
+        public async Task GetProductById_Returns_Product()
+        {
+            using (var client = new TestClientProvider().Client)
+            {
+                var productResponse = await client.GetAsync($"/api/product/getbyid?id={_fixture.Product.Id}");
+
+                using (var responseStream = await productResponse.Content.ReadAsStreamAsync())
+                {
+                    var product = await JsonSerializer.DeserializeAsync<Product>(responseStream,
+                        new JsonSerializerOptions() { PropertyNameCaseInsensitive = true });
+
+                    Assert.Equal(_fixture.Product.Id, product.Id);
+                }
             }
         }
 
